f38a381d3710d0912d3ff98fc8f71e2b2ce2b9dd
LeeHemingway <coherence0815@gmail.com>
2018-10-27 11:38:36 +0900

    add single quote

--- shell/basic/variable.sh
+++ shell/basic/variable.sh
@@ -1,4 +1,8 @@
 VALUE1="Four"  #can't have space
  
-echo "$VALUE1 score and 7 years ago...."
+echo "$VALUE1 score"
+
+# it will show $VALUE1 score
+#  variable does not work with ''
+echo '$VALUE1 score'
 
