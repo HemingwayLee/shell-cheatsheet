8c87374b757ad9818859f26026b8dc5a6d307e03
LeeHemingway <coherence0815@gmail.com>
2020-11-14 14:57:11 +0900

    move folders

--- shell/psql/sql/raise_notice.sql
+++
@@ -1,9 +0,0 @@
-DO language plpgsql $$
-BEGIN
-  RAISE NOTICE 'hello, world!';
-  RAISE NOTICE 'hello, world! %', 5566;
-END
-$$;
-
--- ref:
--- https://stackoverflow.com/questions/18828127/how-to-raise-a-notice-in-postgresql
